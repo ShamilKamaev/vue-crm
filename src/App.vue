9f412115950e12d678e98e2e370c951488dae220
Apple <apple@MacBook-Pro-Apple.local>
2020-02-16 02:14:56 +0300

    added views

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="app">
-    <button class="btn">Test</button>
-    <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
+
+<script>
+import EmptyLayout from '@/layouts/EmptyLayout'
+import MainLayout from '@/layouts/MainLayout'
+export default {
+  components: {
+    EmptyLayout,
+    MainLayout
+  },
+
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || 'main') + '-layout'
+    }
+  }
+}
+</script>
 
 <style lang="scss">
   @import '~materialize-css/dist/css/materialize.min.css';
--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,54 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: () => import('../views/Ctagories.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/planing',
+    name: 'planing',
+    component: () => import('../views/Planing.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    component: () => import('../views/Record.vue')
+  },
 ]
 
 const router = new VueRouter({
